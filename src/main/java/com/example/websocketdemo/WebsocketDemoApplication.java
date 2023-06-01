aae5b5d10838ce4abfd373e0c9367efcc99359db
Sylvester <sylvestre.ofueu@exquisappfactory>
2023-06-02 00:56:33 +0100

    update readme

--- src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
+++ src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -9,4 +9,10 @@ public class WebsocketDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketDemoApplication.class, args);
 	}
+
+	// Learn More
+
+	//You can find the tutorial for this application on my blog -
+
+	//https://www.callicoder.com/spring-boot-websocket-chat-example/
 }
